2063f10d8621f3899186caabaa74c18471d7aea7
Steve Macenski <stevenmacenski@gmail.com>
2020-07-22 12:50:56 -0700

    Fix copyright date

--- src/tinkerforge_imu_ros.cpp
+++ src/tinkerforge_imu_ros.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2029 Steve Macenski
+// Copyright (c) 2020 Steve Macenski
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
